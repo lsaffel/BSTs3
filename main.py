7d9cf013d8f3da4a8783870a031e1b131a2ffb7c
Lisa <llvvss@icloud.com>
2023-09-16 18:12:50 -0600

    rewrote the insert, which was not working quite right. It is now

--- main.py
+++ main.py
@@ -10,36 +10,39 @@ class TreeNode:
         self.right = right
 
 
-def insertIntoBST(self: TreeNode, val: int) -> TreeNode:
-    new_node = TreeNode(val)
-    if self is None:
-        root = new_node
-        return root
-
-    if val < self.val:
-        if self.left is None:
-            self.left = new_node
-        else:
-            self.insertIntoBST(self.left, val)
-    else:       # val > root.val
-        if self.right is None:
-            self.right = new_node
-        else:
-            self.insertIntoBST(self.right, val)
-
-    return self
+# def insertIntoBST(root_ptr: TreeNode, val: int) -> TreeNode:
+#     new_node = TreeNode(val)
+#     if root_ptr is None:
+#         root = new_node
+#         return root
+#
+#     if val < root_ptr.val:
+#         root_ptr = insertIntoBST(root_ptr.left, val)
+#     else:       # val >= root.val
+#         root_ptr = insertIntoBST(root_ptr.right, val)
+#
+#     return root_ptr
 # ---------------------------------------------------------------
+def insertIntoBST(node, val):
+    # return a new node if the tree is empty
+    if node is None:
+        return TreeNode(val)
 
+    if val < node.val:
+        node.left = insertIntoBST(node.left, val)
+    else:
+        node.right = insertIntoBST(node.right, val)
 
-def levelOrder(root):
+    return node
+
+def levelOrder(root_ptr):
     # prints the nodes of the tree in breadth-first order, i.e. each level
     print("Doing a level order traversal of the tree, starting now ----------")
     q = []      # an empty queue, represented by a list
-    print("The root node's value is: ", root.val)
+    # print("The root node's value is: ", root.val)
     valuesList = []         # initialize the list that holds the tree's nodes, traversed breadth first
 
-    q.append(root)
-    print("The queue's first element is now: ", q[0].val)
+    q.append(root_ptr)
 
     while q:        # while q is not empty
         current_node = q.pop(0)
@@ -54,16 +57,35 @@ def levelOrder(root):
             q.append(current_node.right)
 
     print("The list of node values is now: ", valuesList)
-    print("The queue is now: ", q)
 
     # -------------------------------------------------------------------
 
 
 if __name__ == '__main__':
     myTree = TreeNode(17)
-    print(myTree.val)
+    # print(myTree.val)
     levelOrder(myTree)     # prints the nodes of the tree in breadth-first order, i.e. each level
     myTree = insertIntoBST(myTree, 25)
     print(myTree.right.val)
 
+    levelOrder(myTree)     # prints the nodes of the tree in breadth-first order, i.e. each level
+
+    myTree = insertIntoBST(myTree, 12)
+    levelOrder(myTree)     # prints the nodes of the tree in breadth-first order, i.e. each level
+
+    print("The root node is: ", myTree.val)
+    print("The left node is: ", myTree.left.val)
+    print("The right node is: ", myTree.right.val)
+
+    thirdTree = insertIntoBST(myTree, 6)
+
+    levelOrder(myTree)     # prints the nodes of the tree in breadth-first order, i.e. each level
+    #
+    myTree = insertIntoBST(myTree, 14)
+    levelOrder(myTree)     # prints the nodes of the tree in breadth-first order, i.e. each level
+
+    myTree = insertIntoBST(myTree, 15)
+    levelOrder(myTree)     # prints the nodes of the tree in breadth-first order, i.e. each level
+
+    myTree = insertIntoBST(myTree, 23)
     levelOrder(myTree)     # prints the nodes of the tree in breadth-first order, i.e. each level
